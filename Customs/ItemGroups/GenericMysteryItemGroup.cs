97aa1829bb9c22d062fc95b667471494fa544a53
Kayla <kailaria86@gmail.com>
2024-01-14 15:53:40 -0600

    Transfer to desktop * ItemGroup & Mystery Dough for the HQ

---
+++ Customs/ItemGroups/GenericMysteryItemGroup.cs
@@ -0,0 +1,18 @@
+﻿using KitchenData;
+using KitchenLib.Customs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenMysteryMenu.Customs.ItemGroups
+{
+    public abstract class GenericMysteryItemGroup : CustomItemGroup
+    {
+        protected abstract string NameTag { get; }
+        protected abstract ItemGroup ExistingGDO { get; }
+        public override string UniqueNameID => Mod.MOD_NAME + " : " + NameTag;
+
+    }
+}
---
+++ Customs/ItemGroups/MysteryDough.cs
@@ -0,0 +1,14 @@
+﻿using KitchenLib.Customs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenMysteryMenu.Customs.ItemGroups
+{
+    public class MysteryDough : CustomItemGroup
+    {
+        public override string UniqueNameID => throw new NotImplementedException();
+    }
+}
