b1ac7cc05ea3ad15a64a4ff1135fb3e6896b4487
Kailaria <kailaria86@gmail.com>
2024-01-22 04:32:34 -0600

    Limits when to Handle New Mystery cards

--- Systems/HandleNewMysteryMenuDish.cs
+++ Systems/HandleNewMysteryMenuDish.cs
@@ -1,22 +1,33 @@
 ﻿using Kitchen;
+using KitchenMysteryMenu.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.Entities;
 
 namespace KitchenMysteryMenu.Systems
 {
     public class HandleNewMysteryMenuDish : RestaurantSystem
     {
+        private EntityQuery MysteryProviders;
+        private EntityQuery NonHandledMenuItems;
+
         protected override void Initialise()
         {
             base.Initialise();
-            // Only run when there are mystery providers..?
+            MysteryProviders = GetEntityQuery(typeof(CItemProvider), typeof(CMysteryMenuProvider));
+            NonHandledMenuItems = GetEntityQuery(new QueryHelper()
+                .All(typeof(CMenuItem), typeof(CDynamicMenuItem))
+                .None(typeof(CMysteryMenuItem), typeof(CNonMysteryMenuItem)));
+            RequireForUpdate(MysteryProviders);
+            RequireForUpdate(NonHandledMenuItems);
         }
 
         protected override void OnUpdate()
         {
+            // TODO: Refer to HandleNewDish system
             // Check for DynamicMenuType = References.DynamicMenuTypeMystery
             // Add CMysteryMenuItem to whatever has CDynamicMenuItem and is actually of type Mystery
         }
