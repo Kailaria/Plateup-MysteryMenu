e4131436e3312e0f6be496c213a025f255e0e681
Kayla Bivens <kailaria86@gmail.com>
2025-07-16 18:51:47 -0500

    [Fully Loaded] Fixes Probabilities for Desserts, Sides, and Starters * Ensures that only the number of non-Disabled menu items will affect the probability of whether customers will individually order a Side or Starter or as a group order Desserts.

---
+++ Patches/GroupHandleChoosingOrder_Patch.cs
@@ -0,0 +1,76 @@
+﻿using HarmonyLib;
+using Kitchen;
+using KitchenData;
+using KitchenLib.Utils;
+using KitchenMysteryMenu.Components;
+using Sirenix.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+
+namespace KitchenMysteryMenu.Patches
+{
+    [HarmonyPatch(typeof(GroupHandleChoosingOrder))]
+    public class GroupHandleChoosingOrder_Patch
+    {
+        private static object[] StarterPhaseParameters = new object[]
+        {
+            new [] { new QueryHelper().All(typeof(CMenuItem),typeof(CMenuItemStarter))
+                .None(typeof(CDisabledMysteryMenu)).Build() }
+        };
+        private static object[] MainPhaseParameters = new object[]
+        {
+            new [] { new QueryHelper().All(typeof(CMenuItem),typeof(CMenuItemMain))
+                .None(typeof(CDisabledMysteryMenu)).Build() }
+        };
+        private static object[] DessertsPhaseParameters = new object[]
+        {
+            new [] { new QueryHelper().All(typeof(CMenuItem),typeof(CMenuItemDessert))
+                .None(typeof(CDisabledMysteryMenu)).Build() }
+        };
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Initialise")]
+        public static void Initialise_Postfix(ref GroupHandleChoosingOrder __instance)
+        {
+            // Add CDisabled components to ensure only truly available mystery .
+            Type t_CSB = typeof(ComponentSystemBase);
+            MethodInfo m_GetEntityQuery = t_CSB.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(mi => mi.Name.Equals("GetEntityQuery") && mi.GetParameters().Any(p => p.ParameterType == typeof(EntityQueryDesc[])))
+                .FirstOrDefault();
+            var methods = t_CSB.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance).Where(mi => mi.Name.Equals("GetEntityQuery")).ToList();
+            Mod.Logger.LogInfo($"Method match count: {methods.Count}");
+            foreach ( MethodInfo mi in methods ) { Mod.Logger.LogInfo($"t_CSB method: \"{mi}\""); }
+
+            try
+            {
+                var StartersQuery = (EntityQuery) m_GetEntityQuery.Invoke(__instance, StarterPhaseParameters);
+
+                var MainsQuery = (EntityQuery) m_GetEntityQuery.Invoke(__instance, MainPhaseParameters);
+
+                var DessertsQuery = (EntityQuery) m_GetEntityQuery.Invoke(__instance, DessertsPhaseParameters);
+
+                var MenuItems = new Dictionary<MenuPhase, EntityQuery>()
+                {
+                    { MenuPhase.Starter, StartersQuery },
+                    { MenuPhase.Main, MainsQuery },
+                    { MenuPhase.Dessert, DessertsQuery }
+                };
+                ReflectionUtils.GetField<GroupHandleChoosingOrder>("MenuItems", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .SetValue(__instance, MenuItems);
+
+
+            } catch (Exception e)
+            {
+                Mod.Logger.LogError("GroupHandleChoosingOrder_Initialise_Postfix failed");
+                Mod.Logger.LogError($"m_GetEntityQuery = {m_GetEntityQuery}");
+                Mod.Logger.LogException(e);
+                throw e;
+            }
+        }
+    }
+}
---
+++ Patches/GroupHandleReadyToOrder_Patch.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+using Kitchen;
+using KitchenLib.Utils;
+using KitchenMysteryMenu.Components;
+using Sirenix.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+
+namespace KitchenMysteryMenu.Patches
+{
+    [HarmonyPatch(typeof(GroupHandleReadyToOrder))]
+    public class GroupHandleReadyToOrder_Patch
+    {
+        private static object[] SidesParameters = new object[]
+        {
+            new [] { new QueryHelper().All(typeof(CMenuItemSide))
+                .None(typeof(CDisabledMysteryMenu)).Build() }
+        };
+        private static object[] StartersParameters = new object[]
+        {
+            new [] { new QueryHelper().All(typeof(CMenuItemStarter))
+                .None(typeof(CDisabledMysteryMenu)).Build() }
+        };
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Initialise")]
+        public static void Initialise_Postfix(ref GroupHandleReadyToOrder __instance)
+        {
+            // Add CDisabled components to ensure only truly available mystery .
+            Type t_CSB = typeof(ComponentSystemBase);
+            MethodInfo m_GetEntityQuery = t_CSB.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(mi => mi.Name.Equals("GetEntityQuery") && mi.GetParameters().Any(p => p.ParameterType == typeof(EntityQueryDesc[])))
+                .FirstOrDefault();
+            var methods = t_CSB.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance).Where(mi => mi.Name.Equals("GetEntityQuery")).ToList();
+            Mod.Logger.LogInfo($"Method match count: {methods.Count}");
+            foreach ( MethodInfo mi in methods ) { Mod.Logger.LogInfo($"t_CSB method: \"{mi}\""); }
+
+            try
+            {
+                var SidesQuery = m_GetEntityQuery.Invoke(__instance, SidesParameters);
+                ReflectionUtils.GetField<GroupHandleReadyToOrder>("Sides", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .SetValue(__instance, SidesQuery);
+
+                var StartersQuery = m_GetEntityQuery.Invoke(__instance, StartersParameters);
+                ReflectionUtils.GetField<GroupHandleReadyToOrder>("Starters", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .SetValue(__instance, StartersQuery);
+            } catch (Exception e)
+            {
+                Mod.Logger.LogError("GroupHandleReadyToOrder_Initialise_Postfix failed");
+                Mod.Logger.LogError($"m_GetEntityQuery = {m_GetEntityQuery}");
+                Mod.Logger.LogException(e);
+                throw e;
+            }
+        }
+    }
+}
